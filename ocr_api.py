5a37d328fedc76b0810f9235ea4e0c3d77d7d980
axjing <34448205+axjing@users.noreply.github.com>
2022-03-30 10:22:20 +0800

    Update ocr_api.py

--- ocr_api.py
+++ ocr_api.py
@@ -6,18 +6,19 @@ import numpy as np
 
 st.set_page_config(
     page_title="Anders OCR System",
-    page_icon=":shark:",
+    page_icon="random",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "https://www.extremelycoolapp.com/bug"
+        'Get Help': 'https://mp.weixin.qq.com/mp/homepage?__biz=MzI0OTIwMTY3OA==&hid=2&sn=9e8edebde0f1249563eca4c1864dbd0d&scene=18',
+        'Report a bug': "https://mp.weixin.qq.com/mp/homepage?__biz=MzI0OTIwMTY3OA==&hid=2&sn=9e8edebde0f1249563eca4c1864dbd0d&scene=18",
+        'About': "https://mp.weixin.qq.com/mp/homepage?__biz=MzI0OTIwMTY3OA==&hid=2&sn=9e8edebde0f1249563eca4c1864dbd0d&scene=18"
     }
 )
 st.title('Anders OCR System')
 st.write("小应用：准确高效的完成OCR识别任务，支持中、英、日、韩...等语言")
 st.text("手机访问请把该链接复制到手机浏览器使用")
+@st.cache(suppress_st_warning=True)  
 def image_input():
     if st.sidebar.checkbox('Upload'):
         content_file = st.sidebar.file_uploader("Choose a Content  Image", type=["png", "jpg"])
@@ -30,27 +31,20 @@ def image_input():
         content = Image.open(content_file)
         st.image(content)
         hist=content.histogram()
-
-        print(hist)
         st.line_chart(hist)
         reader = easyocr.Reader(['ch_sim','en'],gpu=False,download_enabled=True) # this needs to run only once to load the model into memory
-        with st.spinner('Wait for it...'):
+        with st.spinner('Wait for OCR...'):
             result = reader.readtext(np.array(content),detail = 0)
-
            
         s=""
         if len(result)==0:
-           
-            st.exception('图片中没有文字内容!')
+            st.warning('图片中没有文字内容!')
         else:    
-   
             for i in result:
                 st.write(i)
                 s+=i+'\n'
-           
             st.info(s)
             st.success('OCR is a success message!')
-
         st.write("Time:{:.2f}".format(time.time()-start))
     else:
         st.warning("Upload an Image OR Untick the Upload Button")
